7e97ee6c881b5d5b4e384ccaa2fe4bbb49f5aa24
Tasmiya Adnan <tasmiyadnan@gmail.com>
2024-05-28 00:41:22 +0500

    Admin Panel Login And Database Layer have been done

--- OnlineBloodBank/Controllers/AdminController.cs
+++ OnlineBloodBank/Controllers/AdminController.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using OnlineBloodBank.Models;
+using OnlineBloodDonation.DataLayer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -6,17 +8,55 @@ using System.Web.Mvc;
 
 namespace OnlineBloodBank.Controllers
 {
+
     public class AdminController : Controller
     {
+        private OnlineBloodBankdbEntities onlineBloodBankDB = new OnlineBloodBankdbEntities();
         // GET: Admin
         public ActionResult Home()
         {
+            if (Session["AdminID"] == null && Session["Name"] == null && Session["Email"] == null)
+            {
+                return RedirectToAction("Login", "Admin");
+            }
             return View();
         }
 
         public ActionResult Login()
         {
-            return View();
+            AdminModelView view = new AdminModelView();
+            return View(view);
         }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult LoginToAdmin(AdminModelView admin)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = onlineBloodBankDB.AdminTables.FirstOrDefault(u => u.Email == admin.Email && u.Password == admin.password);
+                if (user != null)
+                {
+                    // Create a session or authentication ticket here
+                    Session["Name"] = user.Name;
+                    Session["Email"] = user.Email;
+                    Session["AdminID"] = user.ID;
+                    return RedirectToAction("Home", "Admin");
+                }
+                ModelState.AddModelError("", "Invalid username or password.");
+            }
+            return View(admin);
+
+
+        }
+
+        [HttpGet]
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login", "Admin");
+        }
+
     }
 }
---
+++ OnlineBloodBank/Models/AdminModelView.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace OnlineBloodBank.Models
+{
+    public class AdminModelView
+    {
+        [Required(ErrorMessage = "Email is required")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required")]
+        [DataType(DataType.Password)]
+        public string password { get; set; }
+
+    }
+}
